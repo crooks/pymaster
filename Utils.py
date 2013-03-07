369f584226a145e69b2a3c22c8fca62c88bb07e2
Steve Crook <steve@mixmin.net>
2013-03-07 15:37:34 +0000

    Merge mailbox and mixmail to create a generic mail function.  Also add remailer-key responder.

---
+++ Utils.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#
+# vim: tabstop=4 expandtab shiftwidth=4 noautoindent
+#
+# nymserv.py - A Basic Nymserver for delivering messages to a shared mailbox
+# such as alt.anonymous.messages.
+#
+# Copyright (C) 2012 Steve Crook <steve@mixmin.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import Config
+
+def capstring():
+    caps = '$remailer{\"%s\"} = ' % config.get('general', 'shortname')
+    caps += '\"<%s> mix' % config.get('mail', 'address')
+    if config.getboolean('general', 'middleman'):
+        caps += ' middle'
+    if config.getint('pool', 'poolsize') >= 5:
+        caps += ' reord'
+    caps += ' klen%s' % config.getint('general', 'klen')
+    caps += '\";'
+    return caps
+
+config = Config.Config().config
+if (__name__ == "__main__"):
+    print capstring()
