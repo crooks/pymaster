4622c886400edc41e056207cc5948a1a57378a3a
Steve Crook <steve@mixmin.net>
2013-03-17 20:53:31 +0000

    Begin work on a Mixmaster packet encoder.

---
+++ EncodePacket.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python
+#
+# vim: tabstop=4 expandtab shiftwidth=4 noautoindent
+#
+# nymserv.py - A Basic Nymserver for delivering messages to a shared mailbox
+# such as alt.anonymous.messages.
+#
+# Copyright (C) 2012 Steve Crook <steve@mixmin.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import struct
+import sys
+import logging
+from Crypto.Cipher import DES3, PKCS1_v1_5
+from Crypto.Hash import MD5
+from Crypto.PublicKey import RSA
+import Crypto.Random
+from Config import config
+import timing
+
+
+log = logging.getLogger("Pymaster.EncodePacket")
+
+
+class ValidationError(Exception):
+    pass
+
+class FinalHop():
+    """Packet type 1 (final hop):
+       Message ID                     [ 16 bytes]
+       Initialization vector          [  8 bytes]
+    """
+    def __init__(self):
+        self.messageid = Crypto.Random.get_random_bytes(16)
+        self.iv = Crypto.Random.get_random_bytes(8)
+    
+
+class EncryptedHeader():
+    def __init__(self, msg_type):
+        self.make_header(msg_type)
+
+    def make_header(self, msg_type):
+        """Packet ID                            [ 16 bytes]
+           Triple-DES key                       [ 24 bytes]
+           Packet type identifier               [  1 byte ]
+           Packet information      [depends on packet type]
+           Timestamp                            [  7 bytes]
+           Message digest                       [ 16 bytes]
+           Random padding               [fill to 328 bytes]
+        """
+        packetid = Crypto.Random.get_random_bytes(16)
+        des3key = Crypto.Random.get_random_bytes(24)
+        ts_sig = struct.pack('BBBBB', 48, 48, 48, 48, 0)
+        timestamp = ts_sig + struct.pack('<H', timing.epoch_days())
+        if msg_type == 1:
+            info = FinalHop()
+            packet = struct.pack('16s24sB16s8s7s',
+                                 packetid,
+                                 des3key,
+                                 msg_type,
+                                 info.messageid,
+                                 info.iv,
+                                 timestamp)
+        digest = MD5.new(data=packet).digest()
+        packet += digest
+        pad = 328 - len(packet)
+        packet += Crypto.Random.get_random_bytes(pad)
+        assert len(packet) == 328
+        self.des3key = des3key
+        self.info = info
+        self.packet = packet
+
+
+class OuterHeader():
+    """Public key ID                [  16 bytes]
+       Length of RSA-encrypted data [   1 byte ]
+       RSA-encrypted session key    [ 128 bytes]
+       Initialization vector        [   8 bytes]
+       Encrypted header part        [ 328 bytes]
+       Padding                      [  31 bytes]
+    """
+    def __init__(self, rem_data, msg_type):
+        self.rem_data = rem_data
+        self.make_outer(msg_type)
+
+    def make_outer(self, msg_type):
+        # pubring[0]    Email Address
+        # pubring[1]    Key ID (Hex encoded)
+        # pubring[2]    Version
+        # pubring[3]    Capabilities
+        # pubring[4]    Pycrypto Key Object
+        keyid = self.rem_data[1].decode('hex')
+        des3key = Crypto.Random.get_random_bytes(24)
+        pkcs1 = PKCS1_v1_5.new(self.rem_data[4])
+        rsakey = pkcs1.encrypt(des3key)
+        lenrsa =  len(rsakey)
+        assert lenrsa == 128
+        iv = Crypto.Random.get_random_bytes(8)
+        inner = EncryptedHeader(msg_type)
+        desobj = DES3.new(des3key, DES3.MODE_CBC, IV=iv)
+        outer_header = struct.pack('16sB128s8s328s31s',
+                                   keyid,
+                                   lenrsa,
+                                   rsakey,
+                                   iv,
+                                   desobj.encrypt(inner.packet),
+                                   Crypto.Random.get_random_bytes(31))
+        assert len(outer_header) == 512
+        self.inner_header = inner
+        self.outer_header = outer_header
+
+
+class RandHop():
+    def __init__(self, msgobj):
+        self.plain = msgobj.get_payload()
+        self.exitnode()
+        self.mix_format()
+
+    def mix_format(self):
+        length = len(self.plain)
+        newmsg = struct.pack('<L', length)
+        newmsg += self.plain
+        newmsg += Crypto.Random.get_random_bytes(10236 - length)
+        rem_data = self.exitnode()
+        self.header = OuterHeader(rem_data, 1)
+        desobj = DES3.new(self.header.inner_header.des3key,
+                          DES3.MODE_CBC,
+                          IV=self.header.inner_header.info.iv)
+        head = self.header.outer_header + Crypto.Random.get_random_bytes(9728)
+        body = desobj.encrypt(newmsg)
+        assert len(head) == 10240
+        assert len(body) == 10240
+        self.message = head + body
+        
+
+    def exitnode(self):
+        name = chain.randexit()
+        rem_data = pubring[name]
+        return rem_data
+
+
+if (__name__ == "__main__"):
+    logfmt = config.get('logging', 'format')
+    datefmt = config.get('logging', 'datefmt')
+    log = logging.getLogger("Pymaster")
+    log.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(fmt=logfmt, datefmt=datefmt))
+    log.addHandler(handler)
+
+    import email
+    import KeyManager
+    import Chain
+    pubring = KeyManager.Pubring()
+    chain = Chain.Chain()
+    #rem_name = "banana"
+    #rem_data = pubring[rem_name]
+    #header = OuterHeader(rem_data, 1)
+    f = open('/opt/steve/pymaster/testmsg.txt', 'r')
+    msg = email.message_from_file(f)
+    f.close()
+    randhop = RandHop(msg)
